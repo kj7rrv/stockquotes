6f0d995d145f4340b69e5df93c44cfdb29880da0
scoopgracie <scoopgracie@gmail.com>
2020-04-20 08:35:29 -0700

    PyPI release
    2.0.0

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 import setuptools
-with open('README.md', 'r') as fh:
-	long_description = fh.read()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 setuptools.setup(
-	name='stockquotes',
-	version='1.0.5',
-	description='A simple module for retreiving stock data',
-	long_description=long_description,
-	long_description_content_type="text/markdown",
-	packages=setuptools.find_packages(),
-	classifiers=[
-		"Programming Language :: Python :: 3",
-		"Operating System :: OS Independent"
-	],
-	python_requires='>=3.0'
+    name="stockquotes",
+    version="2.0.0",
+    description="A simple module for retreiving stock data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.5",
 )
