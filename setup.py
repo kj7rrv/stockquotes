0d12a89e58e5d25f40da6cd9215dce6f87b0b9d5
Samuel Sloniker <sam@kj7rrv.com>
2021-05-26 13:41:49 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name="stockquotes",
-    version="2.0.1",
+    version="2.0.2",
     description="A simple module for retreiving stock data",
     long_description=long_description,
     long_description_content_type="text/markdown",
